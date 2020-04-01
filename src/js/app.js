88bf03f4de65466de6dca29c9e69bf2cd736fd86
Antonio Voce <antonio.voce@guardian.co.uk>
2020-04-01 12:21:38 +0100

    move custom points to file

--- src/js/app.js
+++ src/js/app.js
@@ -3,53 +3,13 @@ import { select } from 'd3-selection'
 import ScrollyTeller from "./scrollyteller"
 import { updateMap } from './globe.js'
 // import countriesLow from '../assets/countries__.json'
+import customPoints from '../assets/customPoints'
 import pointsWithFeature from '../assets/data'
 
 updateMap(pointsWithFeature[0], pointsWithFeature[0].cases)
 // import * as topojson from 'topojson'
 
-const points = [{
-    area: ['China'],
-    date:'Jan 21'
-}, {
-    area: ['China', 'United States of America'],
-    date:'Jan 22'
-}, {
-    area: ['Macao', 'South Korea'],
-    date:'Jan 23'
-}, {
-    area: ['China', 'Singapore', 'Vietnam'],
-    date:'Jan 24'
-}, {
-    area: ['France'],
-    date:'Jan 25'
-}, {
-    area: ['Malaysia', 'Nepal'],
-    date:'Jan 26'
-}, {
-    area: ['Australia', 'Canada'],
-    date:'Jan 27'
-}, {
-    area: ['Cambodia', 'Germany', 'Sri Lanka'],
-    date:'Jan 28'
-}, {
-    area: ['Finland', 'United Arab Emirates'],
-    date:'Jan 29'
-}, {
-    area: ['India', 'Philippines'],
-    date:'Jan 30'
-}, {
-    area: ['Italy', 'Russia', 'Sweden', 'United Kingdom'],
-    date:'Jan 31'
-}, {
-    area: ['Spain'],
-    date:'Feb 1'
-}, {
-    area: ['Belgium'],
-    date:'Feb 4'
-}];
-
-points.forEach(d => {
+customPoints.forEach(d => {
   let div = select(".scroll-text")
   .append('div')
   .attr('class', 'scroll-text__inner')
--- src/render.js
+++ src/render.js
@@ -3,53 +3,12 @@ import rp from 'request-promise'
 import { writeFileSync } from 'fs'
 import * as topojson from 'topojson'
 import countriesLow from './assets/countries__.json'
+import customPoints from './assets/customPoints'
 import { set } from 'd3-collection'
 
 export async function render() {
     const data = await rp('https://interactive.guim.co.uk/docsdata-test/1QIw3MRZDHT2xsLpZ1p9pa0nH1XydmGx7U3n9B2pESmI.json')
     const json = await JSON.parse(data)
-    
-    const points = [{
-        area: ['China'],
-        date: 'Jan 21'
-    }, {
-        area: ['China', 'United States of America'],
-        date: 'Jan 22'
-    }, {
-        area: ['Macao', 'South Korea'],
-        date: 'Jan 23'
-    }, {
-        area: ['China', 'Singapore', 'Vietnam'],
-        date: 'Jan 24'
-    }, {
-        area: ['France'],
-        date: 'Jan 25'
-    }, {
-        area: ['Malaysia', 'Nepal'],
-        date: 'Jan 26'
-    }, {
-        area: ['Australia', 'Canada'],
-        date: 'Jan 27'
-    }, {
-        area: ['Cambodia', 'Germany', 'Sri Lanka'],
-        date: 'Jan 28'
-    }, {
-        area: ['Finland', 'United Arab Emirates'],
-        date: 'Jan 29'
-    }, {
-        area: ['India', 'Philippines'],
-        date: 'Jan 30'
-    }, {
-        area: ['Italy', 'Russia', 'Sweden', 'United Kingdom'],
-        date: 'Jan 31'
-    }, {
-        area: ['Spain'],
-        date: 'Feb 1'
-    }, {
-        area: ['Belgium'],
-        date: 'Feb 4'
-    }];
-
     
     const dates = Object.keys(json.sheets.main_cases[0]).filter(key => key.indexOf(' ') > -1)
 
@@ -66,7 +25,7 @@ export async function render() {
         }
     })
 
-    const pointsWithCases = points.map(d => {
+    const pointsWithCases = customPoints.map(d => {
         const currentDate = d.date;
         let cases = []
 
