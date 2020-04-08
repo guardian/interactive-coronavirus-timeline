db210a5c42c365af91f6549a718f4909d029a526
tinius <nkommenda@hotmail.com>
2020-04-08 11:32:37 +0100

    fix scroll logic at the end

--- src/js/scrollyteller.js
+++ src/js/scrollyteller.js
@@ -14,13 +14,12 @@ class ScrollyTeller {
         this.transparentUntilActive = config.transparentUntilActive;
         this.bigBoxHeight = config.bigBoxHeight
         this.smallBoxHeight = config.smallBoxHeight
-        // this.allBoxes = $$('.scroll-text__div')
-        this.allBoxes = $$('.scroll-text__inner')
+
 
         const noSmallBoxes = document.querySelectorAll('.scroll-text__inner--half').length 
         const noBigBoxes = document.querySelectorAll('.scroll-text__inner').length - noSmallBoxes
 
-        const height = [...document.querySelectorAll('.scroll-text__inner')].map(d => d.getBoundingClientRect().height).reduce((a, b) => a + b) + 300
+        const height = [...document.querySelectorAll('.scroll-text__inner')].map(d => d.getBoundingClientRect().height).reduce((a, b) => a + b) + 100
 
         this.scrollWrapper.style.height = height + "px";
 
@@ -49,10 +48,12 @@ class ScrollyTeller {
             }
     
             if(bbox.top < (window.innerHeight*(this.triggerTop)) && bbox.bottom > window.innerHeight/2) {
-                // const i = Math.floor(Math.abs(bbox.top - (window.innerHeight*(this.triggerTop)))/bbox.height*this.textBoxes.length);
-                const i = Math.max(0, this.allBoxes.findIndex(el => el.getBoundingClientRect().top > this.triggerTop * window.innerHeight) - 1)
-    
-                if(i !== this.lastI) {
+                
+                //const i = Math.floor(Math.abs(bbox.top - (window.innerHeight*(this.triggerTop)))/bbox.height*this.textBoxes.length);
+
+                const i = $$('.scroll-text__div').findIndex( el => el.getBoundingClientRect().top > this.triggerTop*window.innerHeight ) - 1
+
+                if(i >= 0 && i !== this.lastI) {
                     this.lastI = i; 
                     this.doScrollAction(i);
 
@@ -75,7 +76,7 @@ class ScrollyTeller {
     }
 
     doScrollAction(i) {
-        const trigger = this.triggerPoints.find(d => d.num === i);
+        const trigger = this.triggerPoints.find(d => d.num === i+1);
         if(trigger) {
             trigger.do();
         }
